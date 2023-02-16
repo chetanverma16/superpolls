bc05b8179805033d966c4e8ded8a5cfb7c221879
Chetan Verma <chetan61verma@gmail.com>
2023-02-16 17:57:32 +0530

    Initial Setup

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,29 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import '@/styles/globals.css';
+import type { AppProps } from 'next/app';
+import { Analytics } from '@vercel/analytics/react';
+import type { Session } from 'next-auth';
+import { SessionProvider } from 'next-auth/react';
+import { Provider as RWBProvider } from 'react-wrap-balancer';
+import cx from 'classnames';
+import { Inter } from '@next/font/google';
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+const inter = Inter({
+  variable: '--font-inter',
+  subsets: ['latin'],
+});
+
+export default function MyApp({
+  Component,
+  pageProps: { session, ...pageProps },
+}: AppProps<{ session: Session }>) {
+  return (
+    <SessionProvider session={session}>
+      <RWBProvider>
+        <div className={cx(inter.variable)}>
+          <Component {...pageProps} />
+        </div>
+      </RWBProvider>
+      <Analytics />
+    </SessionProvider>
+  );
 }

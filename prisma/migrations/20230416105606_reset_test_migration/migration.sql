5603757dbf736c8988820858caa86378241af3a3
Chetan Verma <chetan61verma@gmail.com>
2023-04-16 17:12:28 +0530

    removing dummy migrations

--- prisma/migrations/20230416105606_reset_test_migration/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to drop the column `test_migrate` on the `User` table. All the data in the column will be lost.
-
-*/
--- AlterTable
-ALTER TABLE "User" DROP COLUMN "test_migrate";

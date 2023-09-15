78bd60ff76aa39d1be3a506aee819754eab81c9f
Chetan Verma <chetan61verma@gmail.com>
2023-09-15 13:13:07 +0530

    Feat: adding microsoft clarity

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,9 +1,18 @@
 import { Html, Head, Main, NextScript } from "next/document";
+import Script from "next/script";
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <Script id="clarity" strategy="afterInteractive">
+          {`    (function(c,l,a,r,i,t,y){
+        c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+        t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+        y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+    })(window, document, "clarity", "script", "ivfscmsunr");`}
+        </Script>
+      </Head>
       <body>
         <Main />
         <NextScript />

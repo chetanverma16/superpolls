adeaf8ad3732c0c0253bbf29ce047e3e088a7e01
Chetan Verma <chetan61verma@gmail.com>
2023-10-10 17:22:39 +0530

    umami script fix

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,11 +1,10 @@
 import { Html, Head, Main, NextScript } from "next/document";
-import Script from "next/script";
 
 export default function Document() {
   return (
     <Html lang="en">
       <Head>
-        <Script
+        <script
           async
           defer
           src="https://analytics.eu.umami.is/script.js"
